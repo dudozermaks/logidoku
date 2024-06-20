d18bea36935035f7981270353bb9e018cacd7a07
Maks <maks.razrushitel08@gmail.com>
2024-06-20 21:26:50 +0300

    remove deafult test

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,3 @@ pub mod cell;
 pub mod figure;
 pub mod grid;
 pub mod methods;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
